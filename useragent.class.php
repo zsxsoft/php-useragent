2ab2c80389ff09eefbc44a2ee188a7943931a38d
zsx <zsx@zsxsoft.com>
2015-01-30 16:11:00 +0800

    Fix bug

--- useragent.class.php
+++ useragent.class.php
@@ -1,5 +1,21 @@
 <?php
+/**
+ * Useragent Class
+ * @package php-useragent
+ * @author zsx <zsx@zsxsoft.com>
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
 Class UserAgentFactory {
+/**
+ * analyze
+ * @static
+ * @param  string $string         UserAgent String]
+ * @param  string $imageSize      Image Size(16 / 24)
+ * @param  string $imagePath      Image Path
+ * @param  string $imageExtension Image Description
+ * @return UserAgent
+ */
 	public static function analyze($string, $imageSize = null, $imagePath = null, $imageExtension = null) {
 		$class = new UserAgent();
 		$imageSize === null || $class->imageSize = $imageSize;
@@ -74,11 +90,7 @@ class UserAgent {
 	}
 
 	public static function __autoload($className) {
-		if (is_file('./lib/' . $className . '.php')) {
-			// For PHPUnit
-			require_once './lib/' . $className . '.php';
-		}
-
+		require_once 'lib/' . $className . '.php';
 	}
 
 	public function analyze($string) {
