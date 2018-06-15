565421c88d7cdd0484c6e8aa0d8b5e09f2b189a1
zsx <zsx@zsxsoft.com>
2018-06-15 15:25:54 +0800

    Fix BuildTestCase, use CLI instead of Web

--- tests/BuildTestCase.php
+++ tests/BuildTestCase.php
@@ -7,53 +7,15 @@
  */
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_NOTICE);
-include '../useragent.class.php';
-$list = explode("\n", file_get_contents("useragentstring.txt"));
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>UserAgent</title>
-</head>
-<body>
-	<!--<table>
-		<tr>
-			<th>Browser</th>
-			<th>OS</th>
-			<th>Device</th>
-			<th>Platform</th>
-			<th>UserAgent</th>
-		</tr>-->
-		<?php
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+$list = explode("\n", file_get_contents(dirname(__FILE__) . "/useragentstring.txt"));
 foreach ($list as $value) {
-	//var_dump(trim($value));exit;
-	//$imageSize = rand(1, 2) == 2 ? 24 : 16;
-	//$imagePath = rand(1, 2) == 2 ? ((string) ((int) (rand() * 10000))) . '/' : null;
-	//$imageExtension = rand(1, 2) == 2 ? ".gif" : null;
-	$useragent = UserAgentFactory::analyze(trim($value)); //, $imageSize, $imagePath, $imageExtension);
-	?>
-<pre>			array(
-					array('<?php echo $useragent->useragent ?>'),
-					array('<?php echo $useragent->browser['image'] ?>', '<?php echo $useragent->platform['image'] ?>', '<?php echo $useragent->browser['name'] ?>', '<?php echo $useragent->browser['version'] ?>', '<?php echo $useragent->browser['title'] ?>', '<?php echo $useragent->os['name'] ?>', '<?php echo $useragent->os['version'] ?>', '<?php echo $useragent->os['title'] ?>', '<?php echo $useragent->device['title'] ?>', '<?php echo $useragent->platform['type'] ?>')
-				),
-</pre><!--
-		<tr>
-			<td>
-				<img src="<?php echo $useragent->browser['image'] ?>"/><?php echo $useragent->browser['title'] ?></td>
-			<td>
-				<img src="<?php echo $useragent->os['image'] ?>"/><?php echo $useragent->os['title'] ?></td>
-			<td>
-				<img src="<?php echo $useragent->device['image'] ?>"/><?php echo $useragent->device['title'] ?></td>
-			<td>
-				<img src="<?php echo $useragent->platform['image'] ?>"/><?php echo $useragent->platform['title'] ?></td>
-			<td>
-				<?php echo $useragent->useragent ?></td>
-		</tr>
--->
-		<?php //exit;
+	$useragent = UserAgentFactory::analyze(trim($value));
+?>
+array(
+	array('<?php echo $useragent->useragent ?>'),
+	array('<?php echo $useragent->browser['image'] ?>', '<?php echo $useragent->platform['image'] ?>', '<?php echo $useragent->browser['name'] ?>', '<?php echo $useragent->browser['version'] ?>', '<?php echo $useragent->browser['title'] ?>', '<?php echo $useragent->os['name'] ?>', '<?php echo $useragent->os['version'] ?>', '<?php echo $useragent->os['title'] ?>', '<?php echo $useragent->device['title'] ?>', '<?php echo $useragent->platform['type'] ?>')
+),
+<?php //exit;
 }
-?><!--</table>-->
-
-</body>
-</html>
+?>
